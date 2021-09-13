62e59b09cc7e3736407353fd6da29e91e0d29e90
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-09-13 07:40:49 +0300

    Add positive and negative unit tests for checking the todo description

--- src/todo.test.js
+++ src/todo.test.js
@@ -14,3 +14,13 @@ test('created todo has wrong title', () => {
   const newTodo = new Todo('exercise','swimming', '12-10-2021', 'Medium');
   expect(newTodo.title === 'marching').toBeFalsy();
 });
+
+test('created todo has proper description', () => {
+  const newTodo = new Todo('exercise','swimming', '12-10-2021', 'Medium');
+  expect(newTodo.description).toBe('swimming');
+});
+
+test('check if created todo has wrong description', () => {
+  const newTodo = new Todo('exercise','swimming', '12-10-2021', 'Medium');
+  expect(newTodo.description === 'jogging').toBeFalsy();
+});
