d76fcb6abae391a2641cadde73fbaae093aac06b
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-09-13 07:36:44 +0300

    Add unit test for proper todo title

--- src/todo.test.js
+++ src/todo.test.js
@@ -3,4 +3,9 @@ import { Todo } from './todo';
 test('create constructor creates a todo', () => {
   const newTodo = new Todo('exercise','swimming', '12-10-2021', 'Medium');
   expect(typeof newTodo).toEqual('object');
-})
+});
+
+test('created todo has proper title', () => {
+  const newTodo = new Todo('exercise','swimming', '12-10-2021', 'Medium');
+  expect(newTodo.title).toBe('exercise');
+});
